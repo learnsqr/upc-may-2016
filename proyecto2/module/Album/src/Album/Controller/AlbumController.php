e013d11e8cdb4ef42a21d2929dca2d5b01fdd3c5
Agustin Calderon <agustincl@gmail.com>
2016-05-04 14:21:56 +0200

    Initial upload

---
+++ proyecto2/module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function insertAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function updateAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function deleteAction()
+    {
+        return new ViewModel();
+    }
+}
+    
